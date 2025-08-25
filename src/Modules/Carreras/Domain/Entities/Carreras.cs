eff63922bd39487532ea57cd8448b51bf01e3842
JhinetDanielaPerezTami <194555077+JhinetDanielaPerezTami@users.noreply.github.com>
2025-08-25 17:21:26 -0500

    feat: :sparkles: Se definieron las entidades Genero y Carreras

---
+++ src/Modules/Carreras/Domain/Entities/Carreras.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampusLove_JuanManuelCrispinCastellanos_y_JhinetDanielaPerezTami.src.Modules.Carreras.Domain.Entities
+{
+    public class Carreras
+    {
+         public int IdCarrera { get; set; }
+        public string NombreCarrera { get; set; } = string.Empty; 
+    }
+}
---
+++ src/Modules/Generos/Domain/Entities/Genero.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CampusLove_JuanManuelCrispinCastellanos_y_JhinetDanielaPerezTami.src.Modules.Generos.Domain.Entities
+{
+    public class Genero
+    {
+        public int IdGenero { get; set; }
+        public string NombreGenero { get; set; } = string.Empty; 
+    }
+}
