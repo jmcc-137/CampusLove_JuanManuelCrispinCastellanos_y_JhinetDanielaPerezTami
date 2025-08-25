09bb90b57215380949aea0c90d2006cd01667082
jmcc-137 <jmcrispinc20@gmail.com>
2025-08-25 14:18:08 -0500

    feat: :sparkles: se agrego la conexion con la base de datos

--- Program.cs
+++ Program.cs
@@ -1,10 +1,3 @@
-﻿using CampusLove_JuanManuelCrispinCastellanos_y_JhinetDanielaPerezTami.UTILS;
-
-// var MP = new MenuPrincipal();
-// MP.MostrarMenuPrincipal();
-
-var MI = new MenuInicio();
-MI.MostrarMenuInicio();
-
-var MP = new MenuPrincipal();
-MP.MostrarMenuPrincipal();
+﻿using examen.src.Modules.Shared.Helpers;
+using Microsoft.EntityFrameworkCore;
+var context = DbContextFactory.Create();
